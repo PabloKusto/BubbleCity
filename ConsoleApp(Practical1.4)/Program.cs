ea4f301193e63b6c76b9d56a52756eb2be33c462
Pavel Kustov <pavel-kustov@bk.ru>
2024-09-01 21:27:58 +0300

    FirstCommit

---
+++ ConsoleApp(Practical1.4)/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp_Practical1._4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello,");
+            Console.Write(" World!");
+            Console.WriteLine("Hello,");
+            Console.WriteLine("World!");
+            Console.Write("Нажмите Enter, чтобы завершить процесс.");
+
+
+            while (true)
+            {
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
